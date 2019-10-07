05471b4e5a5661450d8458b6b7524e5a0535a621
Daniela Hernandez <dannyhvalenz@live.com.mx>
2019-10-07 17:49:49 -0500

    Ver inmueble y menu inmuebles listo

--- Inmobiliaria/js/animaciones.js
+++ Inmobiliaria/js/animaciones.js
@@ -1,32 +1,31 @@
-"use strict";
+'use strict';
 function w3_open() {
-    document.getElementById("divAdmin").style.display = "block";
-    document.getElementById("divVisualizar").style.width = "50%";
-    document.getElementById("divAdmin").style.width = "50%";
-    document.getElementById("divAdmin").style.height = "100vh";
-    document.getElementById("divAdmin").style.display = "block";
-    document.getElementById("divAdmin").style.visibility = "visible";
-
-    document.getElementById("divAdmin").innerHTML =
-        '<object type="text/html" data="registro_propiedad.html" ></object>';
+    window.document.getElementById("divAdmin").style.display = "block";
+    window.document.getElementById("divVisualizar").style.width = "50%";
+    window.document.getElementById("divAdmin").style.width = "50%";
+    window.document.getElementById("divAdmin").style.height = "100vh";
+    window.document.getElementById("divAdmin").style.display = "block";
+    window.document.getElementById("divAdmin").style.visibility = "visible";
+    window.document.getElementById("divAdmin").innerHTML =
+        '<object type="text/html" data="registro_propiedad.html" height="100%" width="100%" class="w3-border-left"></object>';
 }
 
 function w3_close() {
-    document.getElementById("divAdmin").style.display = "none";
+    window.document.getElementById("divAdmin").style.display = "none";
 }
 
 function w3_open_sidebar() {
-    document.getElementById("main").style.marginLeft = "25%";
-    document.getElementById("mySidebar").style.width = "25%";
-    document.getElementById("mySidebar").style.display = "block";
+    window.document.getElementById("main").style.marginLeft = "25%";
+    window.document.getElementById("mySidebar").style.width = "25%";
+    window.document.getElementById("mySidebar").style.display = "block";
 
-    document.getElementById("barra_superior").style.width = "75%";
-    document.getElementById("barra_superior").style.float = "right";
+    window.document.getElementById("barra_superior").style.width = "75%";
+    window.document.getElementById("barra_superior").style.float = "right";
 }
 
 function w3_close_sidebar() {
-    document.getElementById("main").style.marginLeft = "0%";
-    document.getElementById("mySidebar").style.display = "none";
-    document.getElementById("barra_superior").style.display = "inline-block";
-    document.getElementById("barra_superior").style.width = "100%";
+    window.document.getElementById("main").style.marginLeft = "0%";
+    window.document.getElementById("mySidebar").style.display = "none";
+    window.document.getElementById("barra_superior").style.display = "inline-block";
+    window.document.getElementById("barra_superior").style.width = "100%";
 }
